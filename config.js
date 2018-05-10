93fc2a4f049585a5d30a09da800164d92d6b98a2
Magdalena Szymczak <mwozniak.kontakt@gmail.com>
2018-05-10 18:38:50 +0200

    Add all files

---
+++ config.js
@@ -0,0 +1,5 @@
+module.exports = {
+	'GOOGLE_CLIENT_ID': '117003942282-1124v9cdler2rhbir97d47st5s13u5qa.apps.googleusercontent.com',
+	'GOOGLE_CLIENT_SECRET': '9y3pgm79s3QWcEvOns4GHB2v',
+	'CALLBACK_URL': 'http://localhost:3000/auth/google/callback'
+};
